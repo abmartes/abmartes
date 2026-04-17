2fe90df51056b58a07534d8b913aa6c360d4d374
abmartes <adriano_mendes@escola.pr.gov.br>
2026-04-17 14:52:59 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,10 @@
-const CACHE_NAME = 'arkanoid-v1';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap'
-];
+const CACHE_NAME = 'mangue-v1';
+const assets = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
