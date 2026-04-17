59a1c75c8dfc61c85d3e0aa722437a981a6e3802
abmartes <adriano_mendes@escola.pr.gov.br>
2026-04-17 11:16:55 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,4 @@
-const cacheName = 'arkanoid-v2';
+const cacheName = 'arkanoid-v-final';
 const assets = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+self.addEventListener('install', e => { e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))); });
